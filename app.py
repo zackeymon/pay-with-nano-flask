5563e9ceb238c07eb2ffbd20c0d20d622d26be43
Zack <zixux@hotmail.com>
2018-07-16 00:08:46 +0100

    flask-login intergration

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(threaded=True)
+    app.run(threaded=True, debug=True)
--- terminal/controllers.py
+++ terminal/controllers.py
@@ -1,7 +1,28 @@
 from flask import Blueprint, request, render_template
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from api.services import validated, initialise_user
+from api.models import User
 
 terminal = Blueprint('terminal', __name__, static_folder='static', template_folder='templates')
+
+login_manager = LoginManager()
+
+
+@terminal.record_once
+def on_load(state):
+    login_manager.init_app(state.app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@terminal.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return render_template('login.html')
 
 
 @terminal.route('/login', methods=['GET', 'POST'])
@@ -11,6 +32,8 @@ def login_page():
 
     # POST
     if validated(username=request.form['username'], password=request.form['password']):
+        this_user = User.query.filter_by(username=request.form['username'])[0]
+        login_user(this_user)
         return 'Logged in!'
     else:
         return 'Wrong username or password!'
@@ -26,3 +49,6 @@ def registration_page():
     return "Registration Success!"
 
 
+@terminal.route('/debug')
+def debug():
+    return current_user.username + current_user.wallet_id
