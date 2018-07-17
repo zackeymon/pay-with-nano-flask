d8b8a3b23324e3534402a7c16a50dd3f57c852cb
Zack <zixux@hotmail.com>
2018-07-17 20:18:04 +0100

    routing + messages for authentication

--- terminal/controllers.py
+++ terminal/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, redirect, url_for, flash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from api.services import validated, initialise_user
 from api.models import User
@@ -19,39 +19,50 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@terminal.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return render_template('login.html')
-
-
 @terminal.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm(request.form)
-    if request.method == 'GET':
-        return render_template('login.html', form=form)
 
     # POST
-    if validated(username=form.username.data, password=form.password.data):
-        this_user = User.query.filter_by(username=form.username.data)[0]
-        login_user(this_user)
-        return 'Logged in!'
-    else:
-        return 'Wrong username or password!'
+    if form.validate_on_submit():
+        if validated(username=form.username.data, password=form.password.data):
+            this_user = User.query.filter_by(username=form.username.data).first()
+            login_user(this_user)
+            return redirect(url_for('.dashboard'))
+        # 2nd validation failed
+        flash('Wrong username or password!')
+
+    # GET, form includes errors
+    return render_template('login.html', form=form)
 
 
 @terminal.route('/register', methods=['GET', 'POST'])
 def registration_page():
     form = RegisterForm(request.form)
-    if request.method == 'GET':
-        return render_template('register.html', form=form)
 
-    # if validated
-    initialise_user(username=form.username.data, password=form.password.data)
+    # POST
+    if form.validate_on_submit():
+        # TODO: try-catch this
+        initialise_user(username=form.username.data, password=form.password.data)
+        flash("Registration success! Please log in.")
+        return redirect(url_for('.login_page'))
 
-    # redirect to login and display msg
-    return "Registration Success!"
+    # GET, form includes errors
+    return render_template('register.html', form=form)
+
+
+@terminal.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', current_user=current_user)
+
+
+@terminal.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Successfully logged out!')
+    return redirect(url_for('.login_page'))
 
 
 @terminal.route('/debug')
--- terminal/forms.py
+++ terminal/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', description='username')
-    password = PasswordField('Password', description='********')
+    username = StringField('Username', description='username', validators=[InputRequired()])
+    password = PasswordField('Password', description='********', validators=[InputRequired()])
     submit = SubmitField('Log in')
 
 
