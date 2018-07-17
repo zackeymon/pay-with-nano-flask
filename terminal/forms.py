261a7526981d73ea2d6619406964551afdedb92e
Zack <zixux@hotmail.com>
2018-07-17 20:32:56 +0100

    Basic validators

--- terminal/forms.py
+++ terminal/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length, EqualTo
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', description='username', validators=[InputRequired()])
-    password = PasswordField('Password', description='********', validators=[InputRequired()])
+    username = StringField('Username', description='username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', description='********', validators=[InputRequired(), Length(min=4, max=32)])
     submit = SubmitField('Log in')
 
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', description='username')
-    email = StringField('Email', description='joe@blogg.com')
-    password = PasswordField('Password', description='********')
-    re_password = PasswordField('Repeat Password', description='********')
+    username = StringField('Username', description='username', validators=[InputRequired(), Length(min=4, max=20)])
+    email = StringField('Email', description='joe@blogg.com', validators=[InputRequired(), Length(min=4, max=32)])
+    password = PasswordField('Password', description='********', validators=[InputRequired(), Length(min=4, max=32), EqualTo('re_password')])
+    re_password = PasswordField('Repeat Password', description='********', validators=[InputRequired(), Length(min=4, max=32), EqualTo('password')])
     submit = SubmitField('Sign up')
