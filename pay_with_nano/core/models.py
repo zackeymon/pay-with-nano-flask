a88a774d3ccc124f6385921be835a12b61a48ce6
Zack <zixux@hotmail.com>
2018-07-27 20:03:52 +0100

    user pin + clean up

--- pay_with_nano/core/models.py
+++ pay_with_nano/core/models.py
@@ -23,7 +23,9 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
     email = db.Column(db.String(32), unique=True)
+
     receiving_address = db.Column(db.String(32))
+    pin = db.Column(db.String(4))
 
     wallet_id = db.Column(db.String(80), unique=True)
     refund_address = db.Column(db.String(80), unique=True)
